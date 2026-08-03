9097069cb4705d5fd040e994ee3194fe14538d86
bmsubra25 <bmsubra25@gmail.com>
2026-08-03 15:50:20 -0700

    Update mqet.py

--- mqet.py
+++ mqet.py
@@ -73,7 +73,7 @@ def infinity_norm(p, range):
 def MQET(commuting_operators, D, r, g, vars):
   u_s_a_list = []
   B = []
-  for s in product(range(D),repeat = r-1):
+  for s in product(range(D),repeat = r):
     p_s = []
     div_functions = []
     for s_k in s:
